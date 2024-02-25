8a5b6d6710b4525becce29046eb6199bff90aff0
MK <m.kanamaru.it@gmail.com>
2024-02-24 18:52:45 -0800

    inital setup

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-let mongoDB = ;
+const userName = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const clusterInfo = process.env.MONGODB_CLUSTER_INFO;
+console.log(userName);
+
+let mongoDB = `mongodb+srv://${userName}:${password}@${clusterInfo}`;
 
 ////EXAMPLE////
 // Make sure to create .env file and use an environment variable
